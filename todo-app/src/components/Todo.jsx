381f7643c72a5e71fbaf339f38423693f9e2c2f0
mehak-1903 <mehakaggarwal1019@gmail.com>
2025-08-23 00:30:11 +0530

    Use useRef to access input field without state

--- todo-app/src/components/Todo.jsx
+++ todo-app/src/components/Todo.jsx
@@ -1,7 +1,23 @@
-import React from "react";
+import React, { useRef } from "react";
 import todo_icon from '../assets/todo_icon.png';
 import TodoItem from "./TodoItem";
+
+
 export default function Todo(){
+
+    const inputRef = useRef();
+
+    // useRef is used to get a reference to the input element.
+    // It allows us to directly access the current value of the input field
+    // without needing to manage it through React state.
+    // When the ADD button is clicked, we read the value using inputRef.current.value.
+
+    const add = () => {
+        const inputText = inputRef.current.value.trim();
+        console.log(inputText)
+
+        
+    }
     return(
         <div className="bg-white place-self-center w-11/12 max-w-md flex flex-col p-7 min-h-[500px] rounded-xl">
             
@@ -13,8 +29,8 @@ export default function Todo(){
 
             {/* ----Input box ------- */}
             <div className="flex items-center my-7 bg-gray-200 rounded-full">
-                <input className="bg-transparent border-0 outline-none flex-1 h-10 pl-6 pr-2 placeholder:text-[#24327a]" type="text" placeholder="Add your task"/>
-                <button className="border-none rounded-full bg-orange-600 w-25 h-10 text-white text-lg font-semi-bold cursor-pointer">ADD</button>
+                <input ref={inputRef} className="bg-transparent border-0 outline-none flex-1 h-10 pl-6 pr-2 placeholder:text-[#24327a]" type="text" placeholder="Add your task"/>
+                <button onClick={add} className="border-none rounded-full bg-orange-600 w-25 h-10 text-white text-lg font-semi-bold cursor-pointer">ADD</button>
             </div>
 
             {/* -----Todo List------ */}
